60e015fe9c291faddca0704c0484e2ab32a27b85
Jonathan <jonathan@thothnet.com>
2019-08-20 12:06:18 -0500

    Header top component

---
+++ src/components/TopHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <header class="header">
+        <img class="header__logo" alt="adext logo" src="../assets/img/Adext-logo-header.svg">
+        
+    </header>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class TopHeader extends Vue {}
+</script>
+
+<style lang="scss">
+    .header {
+        background: #ffffff;
+
+        &__logo {
+            width: 100px;
+        }
+    }
+</style>
